7560510cf287e92e8d6c39f944ff812e8560c2d7
JosueXA <josue.xicotencatl.a@gmail.com>
2023-06-27 00:57:35 -0600

    TRM

---
+++ TRM/intefaz.py
@@ -0,0 +1,18 @@
+import libreria as lb
+
+def ejecutar_convertir_a_dolares( trm: float ) -> None:
+    pesos = float( input("Ingrese la cantidad de pesos: ") )
+    dolares = lb.convetir_a_dolares( pesos, trm)
+    print( pesos,"pesos son", round(dolares,2),"dolares" )
+
+def ejecutar_convertir_a_pesos( trm: float ) -> None:
+    dolares = float( input("Ingrese la cantidad de dolares: ") )
+    pesos = lb.convertir_a_pesos( dolares, trm)
+    print( dolares,"dolares son", round(pesos,2),"pesos" )
+
+def iniciar_aplicacion() -> None:
+    trm = float( input("Ingrese la TRM: ") )
+    ejecutar_convertir_a_dolares( trm )
+    ejecutar_convertir_a_pesos( trm )
+
+iniciar_aplicacion()
---
+++ TRM/libreria.py
@@ -0,0 +1,5 @@
+def convetir_a_dolares( pesos: float, trm: float ) -> float:
+    return pesos / trm
+
+def convertir_a_pesos( dolares: float, trm: float ) -> float:
+    return dolares * trm
